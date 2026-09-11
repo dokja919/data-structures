c47517de66a4164bed3309cd574fbea009e6198f
HughBGrant <112141259+dokja919@users.noreply.github.com>
2026-09-11 16:12:50 +0900

    선택 정렬 추가

--- 00_array/sort.c
+++ 00_array/sort.c
@@ -1,7 +1,6 @@
 #include <stdbool.h>
-// #include <stddef.h>
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 void swap(int *x, int *y)
 {
@@ -9,62 +8,86 @@ void swap(int *x, int *y)
     *x = *y;
     *y = temp;
 }
-void sort_bubble(int *A, size_t n)
+void sort_bubble(int *list, size_t n)
 {
+    if (n < 2) {
+        return;
+    }
     bool flag = false;
 
     for (size_t i = 0; i < n - 1; i++) {
         flag = false;
 
         for (size_t j = 0; j < n - 1 - i; j++) {
-            if (A[j] > A[j + 1]) {
-                swap(&A[j], &A[j + 1]);
+            if (list[j] > list[j + 1]) {
+                swap(&list[j], &list[j + 1]);
                 flag = true;
             }
         }
-        if (flag == true) {
+        if (flag == false) {
             break;
         }
     }
 }
-void sort_insertion(int *A, size_t n)
+void sort_insertion(int *list, size_t n)
 {
+    if (n < 2) {
+        return;
+    }
     for (size_t i = 1; i < n; i++) {
-        int key = A[i];
+        int key = list[i];
         size_t j = i;
 
-        while (j > 0 && key < A[j - 1]) {
-            A[j] = A[j - 1];
+        while (j > 0 && key < list[j - 1]) {
+            list[j] = list[j - 1];
             j--;
         }
 
-        A[j] = key;
+        list[j] = key;
     }
 }
-void print(int *A)
+void sort_selection(int *list, size_t n)
 {
-    for (size_t i = 0; i < 10; i++) {
-        printf("%d ", A[i]);
+    if (n < 2) {
+        return;
+    }
+    for (size_t i = 0; i < n - 1; i++) {
+        size_t min_index = i;
+
+        for (size_t j = i + 1; j < n; j++)
+            if (list[j] < list[min_index]) {
+                min_index = j;
+            }
+        swap(&list[i], &list[min_index]);
+    }
+}
+void print(int *list, size_t n)
+{
+    for (size_t i = 0; i < n; i++) {
+        printf("%d ", list[i]);
     }
     printf("\n");
 }
 int main()
 {
-    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3};
+    int list[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3};
 
     size_t n = 10;
 
-    size_t sort_number = 1;
+    size_t sort_number = 2;
 
     switch (sort_number) {
     case 0:
-        sort_bubble(A, n);
+        sort_bubble(list, n);
         break;
     case 1:
-        sort_insertion(A, n);
+        sort_insertion(list, n);
+        break;
+    case 2:
+        sort_selection(list, n);
         break;
     }
-    print(A);
+    print(list, n);
 
     return 0;
 }
